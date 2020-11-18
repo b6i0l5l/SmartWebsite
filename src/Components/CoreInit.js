01bc9041f4af488f4a47d097fcec39f2e65a9ce7
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-11-17 19:26:44 -0800

    refactor(CoreInit): refactor the initial value of Core file

--- src/Components/CoreInit.js
+++ src/Components/CoreInit.js
@@ -1,10 +1,8 @@
 export default {
   listening: true,
-  showlistening: "Power Off",
   microphone:"red",
   bgcolor:'#282c34',
   time: new Date().toLocaleString(),
-  testbutton:"red",
   username:'',
-  usercommands:[]
+  usercommands:[],
 }
