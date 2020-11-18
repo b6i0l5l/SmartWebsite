752ee42e3d54b1fa2cf6a5f155808b263d2fc2f2
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-11-17 19:27:35 -0800

    refactor(Core): refacotr Core file some variable and include LogOut file

--- src/Components/Core.js
+++ src/Components/Core.js
@@ -6,6 +6,7 @@ import $ from 'jquery';
 import Init from './CoreInit.js';
 import Clock from './Clock.js';
 import GetApi from './GetApi.js';
+import LogOut from './LogOut.js';
 import { useHistory, useLocation} from 'react-router-dom';
 
 
@@ -19,12 +20,12 @@ recognition.lang = "en-US"
 
 //------------------------VOICE RECOGNITION-----------------------------
 
-const utterance = new SpeechSynthesisUtterance("roger that!");
+const confirmCommand = new SpeechSynthesisUtterance("Yes");
 
 //------------------------COMPONENT-----------------------------
 
 const Speech = () => {
-  const [{listening,microphone, bgcolor, commandsAndDevices}, setState] = useState(Init);
+  const [{listening, microphone, bgcolor, commandsAndDevices}, setState] = useState(Init);
   const location = useLocation();
 
   useEffect (() => {
@@ -44,17 +45,14 @@ const Speech = () => {
     for (let i = 0; i < commandsAndDevices.length; i++){
       if (command === commandsAndDevices[i]["command"]){
         await GetApi.getTriggerByCommand(commandsAndDevices[i]["device"]);
-        speechSynthesis.speak(utterance);
-      }
-      else{
-        console.log("no command");
+        speechSynthesis.speak(confirmCommand);
       }
     }
   }
 
   const handleListen = () => {
     if (listening) {
-      setState(state => ({ ...state, microphone:"green"}));
+      setState(states => ({ ...states, microphone:"green"}));
       recognition.start()
       recognition.onend = () => {
         recognition.start()
@@ -62,7 +60,7 @@ const Speech = () => {
     } else {
       recognition.stop()
       recognition.onend = () => {
-        setState(state => ({ ...state, microphone: "red"}));
+        setState(states => ({ ...states, microphone: "red"}));
       }
     }
 
@@ -124,20 +122,21 @@ const Speech = () => {
     history.push(("/DevicesAndCommands"), location.state);
   }
 
-    return (
-        <div>
-          <header className="AppHeader" style={{backgroundColor: bgcolor}}>
-            <Clock></Clock>
-            <div>Click React to speak</div>
-            <button style={{position: "absolute", top: 10, left: 10}} onClick={handleClick}>update commands</button>
-            <button id='microphone-btn' className="CoreButton" onClick={toggleListen}>
-              <img src={logo} className="AppLogo" alt="logo"/>
-            </button>
-            <div id="final" className="CoreFinalScript"></div>
-          </header>
-          <MicIcon fontSize="large" style={{position:"absolute", color:microphone, top:10, right:10}}/>
-        </div>
-    )
+  return (
+      <div>
+        <header className="AppHeader" style={{backgroundColor: bgcolor}}>
+          <Clock></Clock>
+          <div>Click React to speak</div>
+          <button style={{position: "absolute", top: 40, left: 10, width: 150, height: 20}} onClick={handleClick}>Update Commands</button>
+          <LogOut></LogOut>
+          <button id='microphone-btn' className="CoreButton" onClick={toggleListen}>
+            <img src={logo} className="AppLogo" alt="logo"/>
+          </button>
+          <div id="final" className="CoreFinalScript"></div>
+        </header>
+        <MicIcon fontSize="large" style={{position:"absolute", color:microphone, top:10, right:10}}/>
+      </div>
+  )
 }
 
 export default Speech
