720558be83730d2d9518b3b5b5d75bb3df63e3e0
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-11-19 12:53:20 -0800

    feat(build): get new files after running npm start build

---
+++ build/precache-manifest.bdada1653fec5cdfc8958c12b84649c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffa52df933065e179ae9f5ff7a147cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23a0aec68130c6d90a7",
+    "url": "/static/css/main.6dccaac5.chunk.css"
+  },
+  {
+    "revision": "76946cd5da073888c75a",
+    "url": "/static/js/2.a80820ee.chunk.js"
+  },
+  {
+    "revision": "c3a1273baccf65a7783f8d3bab38b7e3",
+    "url": "/static/js/2.a80820ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23a0aec68130c6d90a7",
+    "url": "/static/js/main.e526fac3.chunk.js"
+  },
+  {
+    "revision": "172c0dd1cba431049d16",
+    "url": "/static/js/runtime-main.1c65611e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47d29253a76f64027ec98a1cc318e5f9.js"
+  "/precache-manifest.bdada1653fec5cdfc8958c12b84649c6.js"
 );
 
 self.addEventListener('message', (event) => {
