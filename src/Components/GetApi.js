d9596ed6d8cb1bd4e47c8d79a1dbd26073b1463e
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-11-20 14:07:24 -0800

    refactor(porxy) add proxy in pack.json and refacotor all the api address

--- src/Components/GetApi.js
+++ src/Components/GetApi.js
@@ -1,32 +1,32 @@
 export default{
   async getCommandByDeviceID(username, action){
-    let res = await fetch('http://127.0.0.1:8000/api/getcommand/'+ action+'?username='+username);
+    let res = await fetch('/getcommand/'+ action+'?username='+username);
     let data = await res.json();
     return {"action":action, "command":data['command']};
   },
   async checkUserAccount(user){
-    let res = await fetch('http://127.0.0.1:8000/api/check/useraccount?username='+ user['username']+'&password='+user['password'])
+    let res = await fetch('/check/useraccount?username='+ user['username']+'&password='+user['password'])
     let data = await res.json();
     console.log(data);
     return data;
   },
   async getCommandsByUser(user){
-    let res = await fetch('http://127.0.0.1:8000/api/getallcommands/'+user)
+    let res = await fetch('/getallcommands/'+user)
     let data = await res.json();
     console.log(data);
     return data;
   },
   async getCommandsAndDevicesByUser(user){
-    let res = await fetch('http://127.0.0.1:8000/api/getdevicesinfos/'+user)
+    let res = await fetch('/getdevicesinfos/'+user)
     let data = await res.json();
     console.log(data);
     return data;
   },
   async getTriggerByCommand(action){
-    let res = await fetch('http://127.0.0.1:8000/api/gettrigger/'+action)
-    let data = await res.json();
-    console.log(data);
-    return data;
+    let res = await fetch('/gettrigger/'+action)
+    // let data = await res.json();
+    console.log(res);
+    // return data;
   }
 
 }
--- src/Components/PostApi.js
+++ src/Components/PostApi.js
@@ -1,11 +1,11 @@
 export default{
   postCommand(request){
-    fetch('http://127.0.0.1:8000/api/postcommand', request)
+    fetch('/postcommand', request)
         .then(response => console.log(response))
         .then(re => console.log(re));
   },
   updateCommand(request){
-    fetch('http://127.0.0.1:8000/api/updatecommand', request)
+    fetch('/updatecommand', request)
         .then(response => console.log(response))
         // .then(re => console.log(re));
   }
